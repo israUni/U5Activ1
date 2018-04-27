6c8113b0be3e9b76ae615b8bf2e4776fee5ed741
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2018-04-26 21:06:37 -0500

    Git inicial con la creación de todos los archivos del proyecto

---
+++ app/src/main/java/u5activ1/mov/disp/unam/com/u5activ1/MainActivity.java
@@ -0,0 +1,13 @@
+package u5activ1.mov.disp.unam.com.u5activ1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/u5activ1/mov/disp/unam/com/u5activ1/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package u5activ1.mov.disp.unam.com.u5activ1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
