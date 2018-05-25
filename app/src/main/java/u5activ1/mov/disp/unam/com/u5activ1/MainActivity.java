bb8b02b90e61736e30c7e5020ca68105e07c2dea
samare Web Studio by Israel E Porfirio Moreno <israel.eduardopm@gmail.com>
2018-05-24 22:02:48 -0500

    Se agregan cambios a la apariencia de la App

--- app/src/main/java/u5activ1/mov/disp/unam/com/u5activ1/MainActivity.java
+++ app/src/main/java/u5activ1/mov/disp/unam/com/u5activ1/MainActivity.java
@@ -1,12 +1,34 @@
 package u5activ1.mov.disp.unam.com.u5activ1;
 
 import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WeatherTask d = new WeatherTask();
+        d.execute();
+    }
+
+    private class WeatherTask extends AsyncTask <Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
     }
 }
